5ec565d42af0fc296696cf1825d2062d8b9dc26c
Robertspoon <robertthemcjug@gmail.com>
2022-10-24 11:40:26 -0300

    t4te

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,40 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Arrays");
+            Console.WriteLine();
+
+            //0 Pistol
+            //1 Atom disintegrator
+            //2 Laser rifle
+            //3 very big gun
+            //4 Trench gun
+
+            //current weapon
+            int weapon = 0; //0..4
+
+
+            //ammo
+
+
+            int[] ammo; //array of integers; declaration
+            ammo = new int[5];// ubstantiation
+            ammo[0] = 6;//pistol
+            ammo[1] = 2;
+            ammo[2] = 25;
+            ammo[3] = 3;
+            ammo[4] = 5;
+
+            // show current weapon (without hardcoding)
+            Console.WriteLine(ammo[weapon]);
+            weapon = 3;
+            Console.WriteLine(ammo[weapon]);
+
+            Console.ReadKey(true);
+        }
+        static void ShowCurrentWeaponAmmo()
+        {
+
         }
     }
 }
